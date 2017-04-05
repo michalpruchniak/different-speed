d9194865f38e1c0e0fc529baab8c2472869f51d7
Michal <michal@debian>
2017-04-05 11:49:58 +0200

    m y commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,31 @@
+$.fn.moveIt = function(section){
+  var instances = []
+      this.section = section;
 
+    $(this).each(function(){
+      instances.push(new moveItItem($(this)));
+    });
+    window.onscroll = function(){
+      var scrollTop = $(window).scrollTop(),
+          wrapperPos = section.offset();
+      if(scrollTop > wrapperPos.top){
+        instances.forEach(function(inst){
+          inst.update(scrollTop-wrapperPos.top);
+          console.log(scrollTop);
+        });
+      }
+    }
+  }
+
+    var moveItItem = function(el){
+      this.el = $(el);
+      this.speed = parseInt(this.el.attr('data-speed'));
+    };
+  moveItItem.prototype.update = function(scrollTop){
+    this.el.css('transform', 'translateY(' + -(scrollTop * this.speed/10)+ 'px)');
+  }
+
+  $(function(){
+    var section = $(".wrapper");
+    $('[data-speed]').moveIt(section);
+  });
